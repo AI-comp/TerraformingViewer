0b6277e40d6dc5f4de1ee56ad47e9512c8271c4a
TN <tn821xxx@gmail.com>
2013-06-14 11:25:37 +0900

    埋め込んだリストを、console.logで雑に表示可能に

--- src/showLog1.js
+++ src/showLog1.js
@@ -4,27 +4,40 @@ var Practice;
     // export‚µ‚Ä‰‚ß‚Äƒ‚ƒWƒ…[ƒ‹‚ÌŠO‚ÅŽg‚¦‚é
     var ShowLog1 = (function () {
         function ShowLog1(list) {
+            var _this = this;
             this.list = list;
             this.index = 0;
-            this.timerID = setInterval(this.show, 500);
+            this.timerID = setInterval(function () {
+                return _this.show();
+            }, 500);
         }
         ShowLog1.prototype.show = function () {
             if(this.list.length > this.index) {
-                console.log(this.list[1]);
+                console.log(this.list[this.index]);
                 this.index++;
             }
         };
         return ShowLog1;
     })();
-    Practice.ShowLog1 = ShowLog1;    
+    Practice.ShowLog1 = ShowLog1;    //ClassEnd
+    
 })(Practice || (Practice = {}));
 window.onload = function () {
     var el = document.getElementById('content');
     console.log("--- Start showLog1 ---");
     new Practice.ShowLog1([
-        1, 
-        2, 
-        3
+        [
+            1, 
+            2
+        ], 
+        [
+            3, 
+            0
+        ], 
+        [
+            4, 
+            5
+        ]
     ]);
 };
 //@ sourceMappingURL=showLog1.js.map
--- src/showLog1.ts
+++ src/showLog1.ts
@@ -4,30 +4,30 @@
 module Practice {
      // exportして初めてモジュールの外で使える
     export class ShowLog1 {
-        list: any[];
-        index: number;
-        timerID: number;
+        private list: any[];
+        private index: number;
+        private timerID: number;
         constructor(list: any[]) {
             this.list = list;
             this.index = 0;
             this.timerID = setInterval(
-                this.show, 500);
+                ()=>this.show(), 500);
         }
 
-        show():void {
+        show(): void {
             if (this.list.length > this.index) {
-                console.log(this.list[1]);
+                console.log(this.list[this.index]);
                 this.index++;
             }
         }
 
 
-    }
+    }//ClassEnd
 }
 
 
 window.onload = () => {
     var el = document.getElementById('content');
     console.log("--- Start showLog1 ---");
-    new Practice.ShowLog1([1,2,3]);
+    new Practice.ShowLog1([[1, 2], [3, 0], [4, 5]]);
 };
